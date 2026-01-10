9920a95233a74c57a029d6b5f389ad59401e4f64
timonrieger <mail@timonrieger.de>
2026-01-10 12:26:12 +0100

    dtos

--- server/src/dtos/maintenance.dto.ts
+++ server/src/dtos/maintenance.dto.ts
@@ -1,16 +1,20 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { MaintenanceAction } from 'src/enum';
 import { ValidateEnum, ValidateString } from 'src/validation';
 
 export class SetMaintenanceModeDto {
+  @ApiProperty({ description: 'Maintenance action', enum: MaintenanceAction })
   @ValidateEnum({ enum: MaintenanceAction, name: 'MaintenanceAction' })
   action!: MaintenanceAction;
 }
 
 export class MaintenanceLoginDto {
+  @ApiPropertyOptional({ description: 'Maintenance token' })
   @ValidateString({ optional: true })
   token?: string;
 }
 
 export class MaintenanceAuthDto {
+  @ApiProperty({ description: 'Maintenance username' })
   username!: string;
 }
